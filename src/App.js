fe4f8fb5c1c3ef65f84d1e7dbbd6c258dfa513b1
Mikias-Miessa <mikiasmiessa@gmail.com>
2024-11-12 00:19:07 +0300

    feat: update ui

--- src/App.js
+++ src/App.js
@@ -1,33 +1,46 @@
-import React, { useState } from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import MindARViewer from './mindar-viewer';
-import MindARThreeViewer from './mindar-three-viewer';
+import image from './assets/image1.png';
 
 function App() {
-  const [started, setStarted] = useState(null);
+  useEffect(() => {
+    const videoElement = document.querySelector('video');
+    if (videoElement) {
+      videoElement.style.position = 'absolute';
+      videoElement.style.top = '0';
+      videoElement.style.left = '0';
+      videoElement.style.zIndex = '10';
+      videoElement.style.width = '100%';
+      videoElement.style.height = '100%';
+      videoElement.style.objectFit = 'cover';
+    }
+  }, []);
 
   return (
-    <div className="App">
-      <h1>Example React component with <a href="https://github.com/hiukim/mind-ar-js" target="_blank">MindAR</a></h1>
+    <div className='bg-[#f9e8d6] h-screen w-full p-6'>
+      <div className='flex justify-between mt-10'>
+        <div>
+          <h1 className='text-4xl font-bold tracking-tighter text-[#151547]'>
+            SCAN QR TO
+          </h1>
+          <h1 className='text-4xl font-bold tracking-tighter text-[#151547]'>
+            CONNECT
+          </h1>
+        </div>
 
-      <div className="control-buttons">
-        {started === null && <button onClick={() => {setStarted('aframe')}}>Start AFRAME version</button>}
-        {started === null && <button onClick={() => {setStarted('three')}}>Start ThreeJS version</button>}
-        {started !== null && <button onClick={() => {setStarted(null)}}>Stop</button>}
+        <div>
+          <img src={image} alt='first page img' className='w-20 h-auto' />
+        </div>
       </div>
 
-      {started === 'aframe' && (
-        <div className="container">
-          <MindARViewer/>
-          <video></video>
-        </div>
-      )}
+      <div className='w-full h-2/3 flex justify-center items-center'>
+        <div className='z-50 relative h-[400px] w-[400px]'>
+          <MindARViewer />
 
-      {started === 'three' && (
-        <div className="container">
-          <MindARThreeViewer />
+          <video autoPlay muted playsInline></video>
         </div>
-      )}
+      </div>
     </div>
   );
 }
